4dee4ab1ee3d0c55ca60d1ab3eedf63eee20b6a1
Inkesiter <truongduchainguyen@outlook.com>
2021-06-02 14:13:06 +0700

    fix bug again

--- QRCode/settings.py
+++ QRCode/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 't6uwp#5#zvu6x&uo@!fkevgfy$_mj#k6es%ldl84!vogm%i$b+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['agile-tor-82331.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vietcredit.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
